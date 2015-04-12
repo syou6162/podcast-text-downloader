278a12735a1d3f04b390be76fd9ee1d700b64d62
Yasuhisa Yoshida <syou6162@gmail.com>
2015-04-12 19:10:30 +0900

    昔と差分があったときだけファイルを更新するように変更

--- src/podcast_text_downloader/core.clj
+++ src/podcast_text_downloader/core.clj
@@ -1,10 +1,14 @@
 (ns podcast-text-downloader.core
   (:use clj-xpath.core)
+  (:use [clojure.java.shell :only [sh]])
   (:use [podcast-text-downloader.common :only (get-items-from-rss)])
   (:use [podcast-text-downloader.content-fetch.core
          :only (content-fetch-from-scientificamerican)]))
 
-(defn -main [& args]
+(def old-md "old.md")
+(def old-content (slurp old-md))
+
+(defn gen-markdown-file []
   (let [items (-> (slurp "http://rss.sciam.com/sciam/60secsciencepodcast")
                   (xml->doc)
                   (get-items-from-rss
@@ -21,5 +25,13 @@
              "- " (:site-title item) "\n"
              "- " (:pubDate item) "\n"
              (:content item))))
-         (clojure.string/join "\n")
-         (println))))
+         (clojure.string/join "\n"))))
+
+(defn convert-to-docx []
+  (sh "pandoc" "-f" "markdown+ignore_line_breaks" old-md "-o" "test.docx"))
+
+(defn -main [& args]
+  (let [md (gen-markdown-file)]
+    (when-not (= md old-content)
+      (spit old-md md)
+      (convert-to-docx))))
