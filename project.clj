da91b7bd4f703cfd8c61f8e20e5041b1fc004079
Yasuhisa Yoshida <syou6162@gmail.com>
2015-04-12 17:50:03 +0900

    initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject podcast-text-downloader "0.0.1"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.github.kyleburton/clj-xpath "1.4.3"]])
