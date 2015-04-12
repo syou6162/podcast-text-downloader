b28bc32115606a28fcb9c8974b37585a6fd616ce
Yasuhisa Yoshida <syou6162@gmail.com>
2015-04-12 17:54:10 +0900

    RSSから必要な要素を抜いてくる関数を追加

---
+++ src/podcast_text_downloader/common.clj
@@ -0,0 +1,19 @@
+(ns podcast-text-downloader.common
+  (:use clj-xpath.core))
+
+(defn get-items-from-rss [rss-doc content-fetch-fn]
+  (let [title ($x:text "/rss/channel/title" rss-doc)]
+    (->> ($x "/rss/channel/item" rss-doc)
+         (take 3)
+         (map
+          (fn [item]
+            (let [link ($x:text "./link" item)
+                  item {:site-title title
+                        :title ($x:text "./title" item)
+                        :link link
+                        :pubDate ($x:text "./pubDate" item)}
+                  content (-> (content-fetch-fn item)
+                              (clojure.string/replace #"<br />" "\n"))]
+              (-> item
+                  (assoc :content content)))))
+         (vec))))
