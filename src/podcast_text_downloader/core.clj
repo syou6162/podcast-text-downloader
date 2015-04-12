cc521135114c56bb71dcd4586715c4474f640ddf
Yasuhisa Yoshida <syou6162@gmail.com>
2015-04-12 17:55:18 +0900

    scientificamericanから取ってくる例を追加

---
+++ src/podcast_text_downloader/core.clj
@@ -0,0 +1,22 @@
+(ns podcast-text-downloader.core
+  (:use [podcast-text-downloader.content-fetch.core
+         :only (content-fetch-from-scientificamerican)]))
+
+(let [items (-> (slurp "http://rss.sciam.com/sciam/60secsciencepodcast")
+                (xml->doc)
+                (get-items-from-rss
+                 (fn [item]
+                   (-> item
+                       :link
+                       slurp
+                       content-fetch-from-scientificamerican))))]
+  (->> items
+       (map
+        (fn [item]
+          (str
+           "# " (:title item) "\n"
+           "- " (:site-title item) "\n"
+           "- " (:pubDate item) "\n"
+           (:content item))))
+       (clojure.string/join "\n")
+       (println)))
