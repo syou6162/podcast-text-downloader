5a933f0b8ea2aea6ba6a9ac1bb897bb3011d5b86
Yasuhisa Yoshida <syou6162@gmail.com>
2015-04-12 18:58:21 +0900

    added main

--- src/podcast_text_downloader/core.clj
+++ src/podcast_text_downloader/core.clj
@@ -4,21 +4,22 @@
   (:use [podcast-text-downloader.content-fetch.core
          :only (content-fetch-from-scientificamerican)]))
 
-(let [items (-> (slurp "http://rss.sciam.com/sciam/60secsciencepodcast")
-                (xml->doc)
-                (get-items-from-rss
-                 (fn [item]
-                   (-> item
-                       :link
-                       slurp
-                       content-fetch-from-scientificamerican))))]
-  (->> items
-       (map
-        (fn [item]
-          (str
-           "# " (:title item) "\n"
-           "- " (:site-title item) "\n"
-           "- " (:pubDate item) "\n"
-           (:content item))))
-       (clojure.string/join "\n")
-       (println)))
+(defn -main [& args]
+  (let [items (-> (slurp "http://rss.sciam.com/sciam/60secsciencepodcast")
+                  (xml->doc)
+                  (get-items-from-rss
+                   (fn [item]
+                     (-> item
+                         :link
+                         slurp
+                         content-fetch-from-scientificamerican))))]
+    (->> items
+         (map
+          (fn [item]
+            (str
+             "# " (:title item) "\n"
+             "- " (:site-title item) "\n"
+             "- " (:pubDate item) "\n"
+             (:content item))))
+         (clojure.string/join "\n")
+         (println))))
