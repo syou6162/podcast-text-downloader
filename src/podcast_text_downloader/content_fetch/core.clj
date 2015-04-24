7c99de08da553ae27acc7e8e2a2e9514ea91fa23
Yasuhisa Yoshida <syou6162@gmail.com>
2015-04-24 14:32:34 +0900

    コメントアウトを削除

--- src/podcast_text_downloader/content_fetch/core.clj
+++ src/podcast_text_downloader/content_fetch/core.clj
@@ -13,8 +13,3 @@
                      (re-find regex)
                      (second))]
     content))
-
-;; (-> "http://www.bbc.co.uk/learningenglish/persian/features/6-minute-english/ep-150326"
-;;     (slurp)
-;;     (content-fetch-from-6-minutes-english)
-;;     (println))
